5534a06f246090747169375414f055fd08f8dbc9
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-05-27 15:34:54 +0100

    assets

---
+++ assets/svg/PlayIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PlayIcon = () => {
+  return (
+    <svg
+      width="28"
+      height="30"
+      viewBox="0 0 28 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.75 12.904C28.4167 13.8355 28.4167 16.1645 26.75 17.096L4.25 29.6721C2.58333 30.6037 0.5 29.4392 0.5 27.5761L0.500001 2.42388C0.500001 0.560755 2.58333 -0.603698 4.25 0.327866L26.75 12.904Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default PlayIcon;
